43a3e86b113c328c558a0f98780339882a2980dc
kim-uhong <kmwhng623@naver.com>
2023-01-15 20:57:16 +0900

    조회한 빈이 모두 필요할 때, List, Map

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,6 +1,5 @@
 package hello.core.autowired;
 
-import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -8,10 +7,10 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.List;
 import java.util.Map;
 
 public class AllBeanTest {
@@ -32,13 +31,11 @@ public class AllBeanTest {
 
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
-        private final List<DiscountPolicy> policies;
 
-        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap) {
             this.policyMap = policyMap;
-            this.policies = policies;
             System.out.println("policyMap = " + policyMap);
-            System.out.println("policies = " + policies);
         }
 
         public int discount(Member member, int price, String discountCode) {
